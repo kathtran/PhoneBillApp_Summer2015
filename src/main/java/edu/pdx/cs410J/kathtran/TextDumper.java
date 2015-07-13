3e45db5dabf2d5dd33bbf3b95eec117204d53167
Kathleen Tran <kathleenvdt@gmail.com>
2015-07-12 22:07:46 -0700

    Set up skeleton for Project 2

---
+++ src/main/java/edu/pdx/cs410J/kathtran/TextDumper.java
@@ -0,0 +1,21 @@
+package edu.pdx.cs410J.kathtran;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.PhoneBillDumper;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class TextDumper implements PhoneBillDumper {
+    /**
+     * Dumps a phone bill to some destination.
+     *
+     * @param bill
+     */
+    @Override
+    public void dump(AbstractPhoneBill bill) throws IOException {
+
+    }
+}
---
+++ src/main/java/edu/pdx/cs410J/kathtran/TextParser.java
@@ -0,0 +1,20 @@
+package edu.pdx.cs410J.kathtran;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.ParserException;
+import edu.pdx.cs410J.PhoneBillParser;
+
+/**
+ *
+ */
+public class TextParser implements PhoneBillParser {
+    /**
+     * Parses some source and returns a phone bill
+     *
+     * @throws ParserException If the source cannot be parsed
+     */
+    @Override
+    public AbstractPhoneBill parse() throws ParserException {
+        return null;
+    }
+}
